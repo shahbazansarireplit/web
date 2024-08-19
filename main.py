53db86e396c3ee8da6c061912990917562c004e2
shahbazansarireplit <shahbazansarireplit@gmail.com>
2024-08-19 11:19:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,23 @@
 from flask import Flask, request, render_template, redirect, url_for
 import requests
 import time
+import subprocess
+
+def uptime1():
+    raw = subprocess.check_output('uptime').decode("utf8").replace(',', '')
+    days = int(raw.split()[2])
+    if 'min' in raw:
+        hours = 0
+        minutes = int(raw[4])
+    else:
+        hours, minutes = map(int,raw.split()[4].split(':'))
+    totalsecs = ((days * 24 + hours) * 60 + minutes) * 60
+    return totalsecs
+
+def uptime2():  
+    with open('/proc/uptime', 'r') as f:
+        uptime_seconds = float(f.readline().split()[0])
+        return uptime_seconds
 
 app = Flask(__name__)
 
@@ -191,7 +208,7 @@ def send_message():
 
 
                 print(e)
-                print("\n[+] All messages sent. Restarting the process...\n")
+                time.sleep(30)
 
     return redirect(url_for('index'))
 
